b584f7b359f4d99475925dd1a2278b8d108da5c7
AnaisDiGenua <anais.digenua5@gmail.com>
2022-02-18 18:37:20 +0100

    add categories hsow

---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h2>{{$category->name}}</h2>
+                </div>
+
+                <div class="card-body">
+                    <div>
+                        Slug : {{$category->slug}}
+                    </div>
+                    <div class="mt-4 d-flex">
+                        {{-- bottone modifica --}}
+                        <a href="{{route("categories.edit", $category->id)}}"><button type="button" class="btn btn-warning mr-2">modifica</button></a>
+                        {{-- bottone elimina --}}
+                        <form action="{{route("categories.destroy", $category->id)}}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit" class="btn btn-danger">elimina</button>
+                        </form>
+                    </div>
+                    <div class="mt-5">
+                        Post associati: 
+                        <ul>
+                            @foreach ($category->posts as $post)
+                                <li>{{$post->title}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
